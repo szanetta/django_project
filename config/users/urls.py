48b56337edbaec10db3d2f6c7522bfc51d32c61f
Halucyna <halucyna321@gmail.com>
2024-06-19 12:26:06 +0200

    added sign in & sign out form

--- config/users/urls.py
+++ config/users/urls.py
@@ -5,4 +5,6 @@ app_name = 'users'
 
 urlpatterns = [
     path('sign_up/', views.sign_up_view, name='sign_up'),
+    path('sign_in/', views.sign_in_view, name='sign_in'),
+    path('sign_out/', views.sign_out_view, name='sign_out'),
 ]
--- config/users/views.py
+++ config/users/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 
 def sign_up_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            login(request, form.save())
             return redirect('pets:list')
     else:
         form = UserCreationForm()
-    return render(request, 'users/sign_up.html', { 'form': form })
+    return render(request, 'users/sign_up.html', {'form': form})
 
+def sign_in_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('pets:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/sign_in.html', {'form': form})
+
+def sign_out_view(request):
+    if request.method == 'POST':
+        logout(request)
+    return redirect('pets:list')
