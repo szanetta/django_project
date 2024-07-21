686f425a6f1eebb1c470fc277aeb909bfb69198b
Halucyna <halucyna321@gmail.com>
2024-07-21 14:28:48 +0200

    ready to deploy

--- config/config/settings.py
+++ config/config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv('MY_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
