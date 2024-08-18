1319659f7f1e47d6d1d6a90ecb1c3a554a61de25
Halucyna <halucyna321@gmail.com>
2024-08-18 18:52:54 +0200

    banner in models altered

--- config/config/settings.py
+++ config/config/settings.py
@@ -148,8 +148,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 
-
-MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#MEDIA_URL = 'media/'
 
 if ENVIRONMENT == 'development':
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
