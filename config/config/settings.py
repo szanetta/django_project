2c91e394f9c906e2798144e8fe181c6071769121
Halucyna <halucyna321@gmail.com>
2024-08-24 17:44:30 +0200

    ready to deploy 25

--- config/config/settings.py
+++ config/config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # .env file configuration
 env = environ.Env(DEBUG=(bool, False))
 
-environ.Env.read_env(os.path.join(BASE_DIR/'.env'))
+environ.Env.read_env(os.path.join(BASE_DIR/'config/.env'))
 
 
 
