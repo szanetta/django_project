11ea964d749fcedfdec522d195895d14c91e0f2d
Halucyna <halucyna321@gmail.com>
2024-08-25 18:23:41 +0200

    ready to deploy 33

--- config/pets/models.py
+++ config/pets/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 class Pet(models.Model):
     name = models.CharField(max_length=45)
@@ -36,7 +37,8 @@ class Pet(models.Model):
             self.slug = slugify(self.name + '-' + str(self.size) + '-' + self.breed + '-' + str(self.gender)+ '-' + str(self.age))
         return super().save(*args, **kwargs)
 
-    banner = models.ImageField(upload_to='pets', default="fallback.PNG", blank=True)
+    #banner = models.ImageField(upload_to='pets', default="fallback.PNG", blank=True)
+    banner = CloudinaryField('pets', default="fallback.PNG", blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     approved = models.BooleanField('Approved', default=False)
     rejected = models.BooleanField('Rejected', default=False)
